5d0341c07f6aa8fad1ed8e14a8cdc2d26752a0b5
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-03-19 20:39:35 +0530

    added insta link

--- src/Components/Footer/Footer2.jsx
+++ src/Components/Footer/Footer2.jsx
@@ -5,13 +5,19 @@ import { FaLinkedin } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
 
 const Footer2 = () => {
+
+    const insta = "https://www.instagram.com/hobbysubcouncil_hbtu/?igsh=cmFodmdxdnJ4Ymx1"
+    const handleInstaClick = () => {
+        window.open(insta, '_blank');
+    }
+    
     return (
         <div className='w-full h-[10vh] flex items-center px-5 bg-[#246A73] border-t-4 border-[#ddbea8]'>
             <h1 className='text-2xl md:text-3xl font-bold text-[#ddbea8]'>Connect With Us- </h1>
             <div className='ml-auto flex gap-5'>
                 <IoCall className='text-2xl md:text-3xl text-[#ddbea8] hover:scale-110 transition-all cursor-pointer' />
-                <FaInstagram className='text-2xl md:text-3xl text-[#ddbea8] hover:scale-110 transition-all cursor-pointer' />
-                <FaLinkedin className='text-2xl md:text-3xl text-[#ddbea8] hover:scale-110 transition-all cursor-pointer' />
+                <FaInstagram onClick={handleInstaClick} className='text-2xl md:text-3xl text-[#ddbea8] hover:scale-110 transition-all cursor-pointer' />
+{/*                 <FaLinkedin className='text-2xl md:text-3xl text-[#ddbea8] hover:scale-110 transition-all cursor-pointer' /> */}
                 <IoMail className='text-2xl md:text-3xl text-[#ddbea8] hover:scale-110 transition-all cursor-pointer' />
             </div>
         </div>
