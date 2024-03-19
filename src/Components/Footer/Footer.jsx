ea6fa5e9aa2c4272e07191d86efd072131c65326
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-03-19 20:38:23 +0530

    route to events

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 const Footer = () => {
+  const redirectToEvents = () => {
+        window.location.href = '/Events';
+    };
   return (
     <div className='h-[90vh] flex flex-col items-center md:items-start md:flex-row w-[80%] ml-auto mr-auto'>
         <div className='w-[70%] md:w-[48%] h-52 md:h-[70%] md:mt-auto md:mb-auto mt-4 rounded-[40px] md:rounded-[70px] overflow-hidden border-2 border-[#ddbea8] bg-red-600'>
@@ -14,7 +17,7 @@ const Footer = () => {
             <p className='text-xl text-center md:text-left text-[#ddbea8] w-full md:w-[65%] mt-5 md:mt-10'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quo eum non laboriosam earum iste aliquid. Veniam porro architecto officiis?
             </p>
-            <button className='border-2 border-white bg-gray-800 text-[#ddbea8] rounded-[50px] mt-10 px-6 py-4 text-xl font-bold font-serif'>
+            <button onClick={redirectToEvents} className='border-2 border-white bg-gray-800 text-[#ddbea8] rounded-[50px] mt-10 px-6 py-4 text-xl font-bold font-serif'>
                 BOOK NOW
             </button>
         </div>
