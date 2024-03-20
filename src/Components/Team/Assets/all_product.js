5f06b4bc1c803aec3d5efa0e785cfafe31894083
zeolee01 <114869179+zeolee01@users.noreply.github.com>
2024-03-20 14:26:09 +0530

    team chnaged

--- src/Components/Team/Assets/all_product.js
+++ src/Components/Team/Assets/all_product.js
@@ -1,32 +1,62 @@
-import mam from './photo/1.jpeg';
-import harshsir  from './photo/4.jpeg';
-import mohitsir  from './photo/3.jpeg';
-import secretary from './photo/sec.jpg';
-import harsh2 from './photo/harshfinal.jpg'
-import shravan from './photo/convener.jpeg'
-import sukrit from './photo/sukrit.jpg'
+import cor1 from "./photo/2.jpeg"
+import harshsir from "./photo/4.jpeg"
+import mohitsir from "./photo/3.jpeg"
+import secretary from "./photo/sec.jpg"
+import harsh2 from "./photo/harshfinal.jpg"
+import shravan from "./photo/convener.jpeg"
+import sukrit from "./photo/sukrit.jpg"
+import aashika from "./photo/aashika.jpg"
+import anand from "./photo/anand.jpeg"
+import nish from "./photo/nish.jpeg"
+import dev from "./photo/dev.jpeg"
+import hrit from "./photo/hritik.jpeg"
+import isha from "./photo/isha.jpeg"
+import sumit from "./photo/sumit.jpeg"
 
 const teamMembers = {
-  
-  
-  Conveynor:[
-     {
-      id:1, name:'Dr. Shravan Kumar',roll:'Assistance Professor',img:shravan,instagram:"",linkedin:""
-     }
-  ],  
-  Secretary:[
+  Convener: [
     {
-      id:1,name:"Ayushi Rao",roll:'Final B.Tech CHE',img:secretary,instagram:"https://www.instagram.com/_ayuushii_?igsh=ZXM1ZHpuMTh4M2pz",linkedin:"https://www.linkedin.com/in/aayushi-rao-072b4b23b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-    }
+      id: 1,
+      name: "Dr. Shravan Kumar",
+      roll: "Assistance Professor",
+      img: shravan,
+      instagram: "",
+      linkedin: "",
+    },
   ],
-  Co_Ordinators:[
-{
-  id:1,name:'Devvrat Singh Rajput',roll:'3rd EE',instagram:"https://www.instagram.com/rajput__devvrat?igsh=eDg3dm5yZm9xcXU2",linkedin:"https://www.linkedin.com/in/devvrat-singh-3b3595226?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-},
-{
-  id:2,name:'Yatendra Yadav',roll:'3rd EE',instagram:"https://www.instagram.com/yatendra_yadav05?igsh=MXh3dnl0Z3o5enU0cA==",linkedin:"https://www.linkedin.com/in/yatendra-yadav-4b7b6a22b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-}
-  ],           
+  Secretary: [
+    {
+      id: 1,
+      name: "Ayushi Rao",
+      roll: "Final B.Tech CHE",
+      img: secretary,
+      instagram: "https://www.instagram.com/_ayuushii_?igsh=ZXM1ZHpuMTh4M2pz",
+      linkedin:
+        "https://www.linkedin.com/in/aayushi-rao-072b4b23b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+  ],
+  Co_Ordinators: [
+    {
+      id: 1,
+      name: "Devvrat Singh Rajput",
+      roll: "3rd EE",
+      img: dev,
+      instagram:
+        "https://www.instagram.com/rajput__devvrat?igsh=eDg3dm5yZm9xcXU2",
+      linkedin:
+        "https://www.linkedin.com/in/devvrat-singh-3b3595226?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      id: 2,
+      name: "Yatendra Yadav",
+      roll: "3rd EE",
+      instagram:
+        "https://www.instagram.com/yatendra_yadav05?igsh=MXh3dnl0Z3o5enU0cA==",
+      linkedin:
+        "https://www.linkedin.com/in/yatendra-yadav-4b7b6a22b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      img: cor1,
+    },
+  ],
   // Logistics: [
   //   { id: 1, name: 'Divyansh Verma', roll: '3rd BE',instagram:'' },
   //   { id: 2, name: 'Jatin Kaushik', roll: '3rd CHE' },
@@ -62,33 +92,116 @@ const teamMembers = {
   //   { id: 18, name: 'Shivendra Tripathi', roll: '3rd PT' },
   //   { id: 19, name: 'Sneha', roll: '3rd CHE' },
   // ],
-  Design: [
-    { id: 1, name: "Hritik Gupta", roll: "3rd CHE (poster)",instagram:"https://www.instagram.com/i_s_hiii?igsh=MWlwOGRsOGwyZWg4Mg==",linkedin:"https://www.linkedin.com/in/hritik-gupta-836805229" },
-    { id: 2, name: "Isha Singh", roll: "3rd CHE (Poster)" ,instagram:"https://www.instagram.com/_aashina?igsh=MTZjc2p4ajdsdHNu",linkedin:"https://www.linkedin.com/in/isha-singh-35aab41b3"},
-    { id: 3, name: "Aashika Nath", roll: "3rd EE ",img:mam ,instagram:"",linkedin:""},
+  WEBD_HEADS: [
+    {
+      id: 1,
+      name: "Harsh Aggarwal",
+      roll: "3rd ET",
+      img: harshsir,
+      instagram:
+        "https://www.instagram.com/harsh._.agarwaal?igsh=MWE2a3h4eWV3dnEybw==",
+      linkedin:
+        "https://www.linkedin.com/in/harsh-agarwal-68577a229?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      id: 2,
+      name: "Mohit Jaiswal",
+      roll: "3rd ET",
+      img: mohitsir,
+      instagram:
+        "https://www.instagram.com/imohit.jaiswal?igsh=MTR4bnRpam5iOTV0eQ==",
+      linkedin:
+        "https://www.linkedin.com/in/mohit-jaiswal-190b23271?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      id: 3,
+      name: "Anand Tiwari",
+      roll: "3rd EE",
+      img: anand,
+      instagram:
+        "https://www.instagram.com/anand_tiwar_i?igsh=MWhpcjZnM3k2cXpwdg==",
+      linkedin:
+        "https://www.linkedin.com/in/anand-tiwari-4286b4226?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      id: 4,
+      name: "Nishant Kardam",
+      roll: "3rd EE",
+      img: nish,
+      instagram:
+        "https://www.instagram.com/_.nishant.kardam?igsh=Ynk2MHJ2ajFkcTF0",
+      linkedin:
+        "https://www.linkedin.com/in/nishant-kardam-70299022b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
   ],
-  WEBD_HEADS:[
-    { id: 1, name: "Harsh Aggarwal", roll: "3rd ET",img:harshsir ,instagram:"https://www.instagram.com/harsh._.agarwaal?igsh=MWE2a3h4eWV3dnEybw==",linkedin:"https://www.linkedin.com/in/harsh-agarwal-68577a229?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"},
-    { id: 2, name: "Mohit-Jaiswal", roll: "3rd ET",img:mohitsir ,instagram:"https://www.instagram.com/imohit.jaiswal?igsh=MTR4bnRpam5iOTV0eQ==",linkedin:"https://www.linkedin.com/in/mohit-jaiswal-190b23271?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"},
-    { id: 3, name: "Anand Tiwari", roll: "3rd EE",instagram:"https://www.instagram.com/anand_tiwar_i?igsh=MWhpcjZnM3k2cXpwdg==",linkedin:"https://www.linkedin.com/in/anand-tiwari-4286b4226?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
-    { id: 4, name: "Nishant Kardam", roll: "3rd EE",instagram:"https://www.instagram.com/_.nishant.kardam?igsh=Ynk2MHJ2ajFkcTF0",linkedin:"https://www.linkedin.com/in/nishant-kardam-70299022b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"},
+  Design: [
+    {
+      id: 3,
+      name: "Aashika Nath",
+      roll: "3rd EE ",
+      instagram: "",
+      linkedin: "",
+      img: aashika,
+    },
+    {
+      id: 1,
+      name: "Hritik Gupta",
+      roll: "3rd CHE (poster)",
+      img: hrit,
+      instagram: "https://www.instagram.com/i_s_hiii?igsh=MWlwOGRsOGwyZWg4Mg==",
+      linkedin: "https://www.linkedin.com/in/hritik-gupta-836805229",
+    },
+    {
+      id: 2,
+      name: "Isha Singh",
+      roll: "3rd CHE (Poster)",
+      img: isha,
+      instagram: "https://www.instagram.com/_aashina?igsh=MTZjc2p4ajdsdHNu",
+      linkedin: "https://www.linkedin.com/in/isha-singh-35aab41b3",
+    },
   ],
   web_developers: [
-  
-    
-    { id: 1, name: "Harsh Pathak", roll: "2nd CSE",img:harsh2,instagram:"",linkedin:"" },
-    { id: 2, name: "Govind Kumar", roll: "2nd CSE",instagram:"",linkedin:"" },
+    {
+      id: 1,
+      name: "Harsh Pathak",
+      roll: "2nd CSE",
+      img: harsh2,
+      instagram: "",
+      linkedin: "",
+    },
+    {
+      id: 2,
+      name: "Govind Kumar",
+      roll: "2nd CSE",
+      instagram: "",
+      linkedin: "",
+    },
     {
       id: 3,
       name: "Sukrit Prakash",
       roll: "2nd CSE",
       instagram:
         "https://www.instagram.com/sukritprakashsingh?igsh=dTNzbGJzcmd3NTg2",
-        img:sukrit,
-        linkedin:""
+      img: sukrit,
+      linkedin: "",
     },
-    { id: 4, name: "Sumit Singh", roll: "2nd CSE" ,instagram:"",linkedin:""},
-    { id: 5, name: "Harsh Chaturvedi", roll: "2nd ME" ,instagram:"",linkedin:""},
+    {
+      id: 4,
+      name: "Sumit Singh",
+      roll: "2nd CSE",
+      img: sumit,
+      instagram:
+        "https://www.instagram.com/rustygenius_98?igsh=cHJsODdmZzVsYW14",
+      linkedin:
+        "https://www.linkedin.com/in/sumit-singh-developer?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      id: 5,
+      name: "Harsh Chaturvedi",
+      roll: "2nd ME",
+      instagram: "",
+      linkedin: "",
+    },
   ],
 }
 
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,43 +1,48 @@
-import React,{lazy} from 'react';
-import './Team.css';
+import React, { lazy } from "react"
+import "./Team.css"
 
-
-import Hover2 from './Hover/Hover2';
-import Navbar from '../Navbar/Navbar';
-import Footer2 from '../Footer/Footer2';
-import teamMembers from './Assets/all_product';
-import Hover from './Hover/Hover2';
+import Hover2 from "./Hover/Hover2"
+import Navbar from "../Navbar/Navbar"
+import Footer2 from "../Footer/Footer2"
+import teamMembers from "./Assets/all_product"
+import Hover from "./Hover/Hover2"
 
 // const teamMembers = lazy(() => import ('./Assets/all_product')) ;
 // const Footer2= lazy(() => import ('../Footer/Footer2')) ;
 // const  Navbar  = lazy(() => import ('../Navbar/Navbar')) ;
 // const Hover2 = lazy(() => import ('./Hover/Hover2')) ;
 
-
 const Team = () => {
   const renderTeamSection = (teamName, teamMembers) => (
     <section key={teamName} className="team-section">
-      <h2>{teamName}</h2>
+      <h2 style={{ position: "sticky", top: 0 }}>{teamName}</h2>
       <div className="card-container">
         {teamMembers.map((item) => (
           <div key={item.id} className="card-wrapper">
-            <Hover2 instagram={item.instagram} linkedin={item.linkedin} img={item.img} id={item.id} name={item.name} role={item.roll} />
+            <Hover2
+              instagram={item.instagram}
+              linkedin={item.linkedin}
+              img={item.img}
+              id={item.id}
+              name={item.name}
+              role={item.roll}
+            />
           </div>
         ))}
       </div>
     </section>
-  );
+  )
 
   return (
     <>
-    <div className='team-container'>
+      <div className="team-container">
         <h1>Meet Our Teams</h1>
-        {Object.entries(teamMembers).map(([teamName, members]) => 
+        {Object.entries(teamMembers).map(([teamName, members]) =>
           renderTeamSection(teamName, members)
         )}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Team;
+export default Team
